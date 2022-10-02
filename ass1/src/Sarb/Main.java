b8bb0d70cdb1cc070ff53eb5cd0c65bd5e512472
Victor <victorcheng0425@gmail.com>
2022-10-02 16:29:23 -0700

    1

---
+++ ass1/src/Sarb/Main.java
@@ -0,0 +1,48 @@
+package Sarb;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Assignment1 implements NeuralNetInterface {
+
+
+    @Override
+    public double outputFor(double[] X) {
+        return 0;
+    }
+
+    @Override
+    public double train(double[] X, double argValue) {
+        return 0;
+    }
+
+    @Override
+    public void save(File argFile) {
+
+    }
+
+    @Override
+    public void load(String argFileName) throws IOException {
+
+    }
+
+    @Override
+    public double sigmoid(double x) {
+        return 0;
+    }
+
+    @Override
+    public double customSigmoid(double x) {
+        return 0;
+    }
+
+    @Override
+    public void initializeWeights() {
+
+    }
+
+    @Override
+    public void zeroWeights() {
+
+    }
+}
---
+++ ass1/src/Sarb/NeuralNet.java
@@ -0,0 +1,2 @@
+package Sarb;public class NeuralNet {
+}
